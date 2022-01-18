88f771a215b40dffbdbaffa4561f13756192da61
juanmarin444 <30785213+Juanmarin444@users.noreply.github.com>
2022-01-17 18:44:26 -0800

    other product-page transitions

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ const tlEnter = gsap.timeline({
 
 
 // Timeline functions
-
 const leaveAnimation = (current, done) => {
   const arrow = current.querySelector('.showcase-arrow')
   const product = current.querySelector('.image-container')
@@ -44,8 +43,35 @@ const enterAnimation = (next, done, gradient) => {
   )
 }
 
-// Run animations
+const productEnterAnimation = (next, done) => {
+  const cards = next.querySelectorAll('.card')
 
+  return (
+    tlEnter.fromTo(next, { y: '100%' }, { y: '0%' }),
+    tlEnter.fromTo(cards, { opacity: 0, y: 50 }, { opacity: 1, y: 0, stagger: .1, onComplete: done })
+  )
+}
+
+const productLeaveAnimation = (current, done) => {
+
+  return (
+    tlLeave.fromTo(current, { y: '0%' }, { y: '100%', onComplete: done })
+  )
+}
+
+// Changing gradient on Showcase
+const getGradient = (name) => {
+  switch(name) {
+    case 'handbag':
+      return 'linear-gradient(260deg, #b75d62, #754d4f)'
+    case 'boot':
+      return 'linear-gradient(260deg, #5d8cb7, #4c4f70)'
+    case 'hat':
+      return 'linear-gradient(260deg, #b27a5c, #7f5450)'
+  }
+}
+
+// Run animations
 barba.init({
   preventRunning: true,
   transitions: [
@@ -72,19 +98,57 @@ barba.init({
         // gsap.fromTo(next, { opacity: 0 }, { opacity: 1, duration: 1, onComplete: done });
         enterAnimation(next, done, gradient)
       }
+    },
+    // Handbag to Product-page transitions
+    {
+      name: 'product-transition',
+      from: { namespace: [ 'handbag', 'product' ] },
+      to: { namespace: [ 'product', 'handbag' ] },
+      sync: true,
+      leave(data) {
+        const done = this.async();
+        let current = data.current.container;
+        productLeaveAnimation(current, done)
+      },
+      enter(data) {
+        const done = this.async();
+        let next = data.next.container;
+        productEnterAnimation(next, done)
+      }
+    },
+    // Hat to Product-page transitions
+    {
+      name: 'product-transition',
+      from: { namespace: [ 'hat', 'product' ] },
+      to: { namespace: [ 'product', 'hat' ] },
+      sync: true,
+      leave(data) {
+        const done = this.async();
+        let current = data.current.container;
+        productLeaveAnimation(current, done)
+      },
+      enter(data) {
+        const done = this.async();
+        let next = data.next.container;
+        productEnterAnimation(next, done)
+      }
+    },
+    // Boot to Product-page transitions
+    {
+      name: 'product-transition',
+      from: { namespace: [ 'boot', 'product' ] },
+      to: { namespace: [ 'product', 'boot' ] },
+      sync: true,
+      leave(data) {
+        const done = this.async();
+        let current = data.current.container;
+        productLeaveAnimation(current, done)
+      },
+      enter(data) {
+        const done = this.async();
+        let next = data.next.container;
+        productEnterAnimation(next, done)
+      }
     }
   ]
 });
-
-
-// Changing gradient on Showcase
-const getGradient = (name) => {
-  switch(name) {
-    case 'handbag':
-      return 'linear-gradient(260deg, #b75d62, #754d4f)'
-    case 'boot':
-      return 'linear-gradient(260deg, #5d8cb7, #4c4f70)'
-    case 'hat':
-      return 'linear-gradient(260deg, #b27a5c, #7f5450)'
-  }
-}
